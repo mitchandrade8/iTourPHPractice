044654f7c7d7ab5e443be507fd431758d0ba4cb8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-14 10:54:24 -0600

    --Progress: Adding UI code on ContentView

--- iTourPHPractice/ContentView.swift
+++ iTourPHPractice/ContentView.swift
@@ -5,17 +5,34 @@
 //  Created by Mitch Andrade on 10/14/23.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Query var destinations: [Destination]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("Trips")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples() {
+        
     }
 }
 
