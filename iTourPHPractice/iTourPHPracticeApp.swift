6ce02153f3da084734bd41a031b93ecbc27d9f07
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-14 10:17:46 -0600

    Initial Commit

---
+++ iTourPHPractice/iTourPHPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourPHPracticeApp.swift
+//  iTourPHPractice
+//
+//  Created by Mitch Andrade on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourPHPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
