979749af84ab02186ede7e81471362903734ea51
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-14 10:42:19 -0600

    --Updating: to building out our dataModel

--- iTourPHPractice/Destination.swift
+++ iTourPHPractice/Destination.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class Destination {
+    var name: String
+    var details: String
+    var date: Date
+    var priority: Int
+    
+    init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
+        self.name = name
+        self.details = details
+        self.date = date
+        self.priority = priority
+    }
+}
