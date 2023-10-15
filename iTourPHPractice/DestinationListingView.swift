9f2e0b039de4b879ea1889a6d528cb8e3b21c0ef
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-15 14:56:27 -0600

    --Progress: extracted the @Query macro and the list from ContentView, as well as the deleteDestination function. Refactoring logic and some UI

---
+++ iTourPHPractice/DestinationListingView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationListingView.swift
+//  iTourPHPractice
+//
+//  Created by Mitch Andrade on 10/15/23.
+//
+
+import SwiftUI
+
+struct DestinationListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationListingView()
+}
