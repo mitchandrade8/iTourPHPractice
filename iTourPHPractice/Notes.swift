60814251fc648a5bbf1632d68e1fa617fd2cb8a2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-15 15:16:56 -0600

    --Progress: Beginning of adding customization of sort functionality and added notes and questions

---
+++ iTourPHPractice/Notes.swift
@@ -0,0 +1,8 @@
+//
+//  Notes.swift
+//  iTourPHPractice
+//
+//  Created by Mitch Andrade on 10/15/23.
+//
+
+import Foundation
---
+++ iTourPHPractice/Questions.swift
@@ -0,0 +1,8 @@
+//
+//  Questions.swift
+//  iTourPHPractice
+//
+//  Created by Mitch Andrade on 10/15/23.
+//
+
+import Foundation
